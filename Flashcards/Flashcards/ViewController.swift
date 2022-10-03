156ed9f8eeea99e9ccb26f70f57b45b1e6face5d
jejack5203 <109096014+jejack5203@users.noreply.github.com>
2022-10-03 17:55:55 -0400

    Add files via upload

---
+++ Flashcards/Flashcards/ViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  Flashcards
+//
+//  Created by Jodie Jackson on 10/2/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+   
+    @IBOutlet weak var frontLabel: UILabel!
+    @IBOutlet weak var backLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func didTapOnFlashcard(_ sender: Any) {
+        frontLabel.isHidden = true
+    }
+    func updateFlashcard(question: String, answer: String){
+       
+        frontLabel.text = question
+        backLabel.text = answer
+      
+    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?){
+        let navigationController = segue.destination as! UINavigationController
+        
+        let creationController = navigationController.topViewController as! CreationViewController
+        
+        creationController.flashcardsController = self
+        
+    }
+}
+
